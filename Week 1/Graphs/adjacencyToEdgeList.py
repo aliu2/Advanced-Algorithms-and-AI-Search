9db673380856c4e3b2ed440dca341783489a7b04
Uzair Ali <34479006+aliu2@users.noreply.github.com>
2018-11-24 20:01:31 +0000

    Delete adjacencyToEdgeList.py

--- Week 1/Graphs/adjacencyToEdgeList.py
+++
@@ -1,8 +0,0 @@
-def make_edge_list(adjacency):
-	edge_list = []
-	for i, lst in enumerate(adjacency):
-		edge_list.append([])
-		for j, num in enumerate(lst):
-			if num == 1:
-				edge_list[i].append(chr(ord("A") + j))
-	return edge_list
